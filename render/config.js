bafbb8a1e454f1fd13b4eaa2a816bc283e1d880d
daffaW <44244449+daffawrdhn@users.noreply.github.com>
2026-07-26 22:13:13 +0700

    remove render client

--- render/config.js
+++
@@ -1,6 +0,0 @@
-window.XOXO_CONFIG = {
-    appName: 'XOXO Chat',
-    appTagline: 'Chat with Stranger Anonymously',
-    appUrl: 'https://chat.1year.site',
-    wsUrl: 'wss://chat.1year.site/ws'
-};
